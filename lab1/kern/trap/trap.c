ff2c775109cb66ced45828058e00f1ce8dd589f5
kioku152 <1830787294@qq.com>
2024-09-27 16:43:31 +0800

    lab1 & .gitignore

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -106,12 +106,23 @@ void interrupt_handler(struct trapframe *tf) {
             // In fact, Call sbi_set_timer will clear STIP, or you can clear it
             // directly.
             // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   YOUR CODE :  */
+             /* LAB1 EXERCISE2   2212285 :  */
             /*(1)设置下次时钟中断- clock_set_next_event()
              *(2)计数器（ticks）加一
              *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
             * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
             */
+           clock_set_next_event();//设置下次时钟中断
+           ticks+=1;//计数器（ticks）加一
+            if (ticks==TICK_NUM) {//计数器加到100
+                print_ticks();
+                ticks=0;
+                num+=1;//打印次数加一
+                if(num==10)//印次数为10
+                    sbi_shutdown();//关机
+            }
+            
+            
             break;
         case IRQ_H_TIMER:
             cprintf("Hypervisor software interrupt\n");
@@ -145,19 +156,28 @@ void exception_handler(struct trapframe *tf) {
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
              // 非法指令异常处理
-             /* LAB1 CHALLENGE3   YOUR CODE :  */
+             /* LAB1 CHALLENGE3   2212285 :  */
             /*(1)输出指令异常类型（ Illegal instruction）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type:Illegal instruction\n");
+            cprintf("Illegal instruction caught at 0x%016llx\n", tf->epc);
+            tf->epc += 4;
+
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
-            /* LAB1 CHALLLENGE3   YOUR CODE :  */
+            /* LAB1 CHALLLENGE3   2212285 :  */
             /*(1)输出指令异常类型（ breakpoint）
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type: breakpoint\n");
+            cprintf("ebreak caught at 0x%016llx\n", tf->epc);
+            tf->epc += 2;
+            
+
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
